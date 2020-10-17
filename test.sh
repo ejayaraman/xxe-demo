cc17984ae59eb2c2bffb7cb1c075c978c12dde0c
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2020-10-17 14:47:36 +0100

    Updated README.md Added xml_external_entity_attack.policy added post change results for insane

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 #Runs the test NO_OF_TIMES before triggering active scan
 NO_OF_TIMES=$1
+ZAP_HOST="localhost:11000"
 
 for i in $(seq 1 ${NO_OF_TIMES:=1}); do
-curl -H "Content-Type: application/xml" -X POST http://localhost:12345/$i -d $'<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<foo>hello</foo>' --proxy localhost:11000
+curl -H "Content-Type: application/xml" -X POST http://localhost:12345/$i -d $'<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<test>hello</test>' --proxy ${ZAP_HOST}
 
-curl -H "Content-Type: application/xml" -X PUT http://localhost:12345/$i -d $'<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<foo>hello</foo>' --proxy localhost:11000
+curl -H "Content-Type: application/xml" -X PUT http://localhost:12345/$i -d $'<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<test>hello</test>' --proxy ${ZAP_HOST}
 done
 
-curl "http://localhost:11000/JSON/ascan/action/scan/?url=http%3A%2F%2Flocalhost%3A12345&scanPolicyName=xml_external_entity_attack"
+echo "Importing XXE Scan Policy"
+#curl -G --data-urlencode path="${PWD}/xml_external_entity_attack.policy" "http://${ZAP_HOST}/JSON/ascan/action/importScanPolicy"
+
+echo "Triggering active scan with XXE scan policy"
+#curl "http://${ZAP_HOST}/JSON/ascan/action/scan/?url=http%3A%2F%2Flocalhost%3A12345&scanPolicyName=xml_external_entity_attack"
 
 
