cbc19ca1991cd8a941c21bffe0437069fb332da9
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2020-10-24 14:25:03 +0100

    another comment

--- app/controllers/XxeController.scala
+++ app/controllers/XxeController.scala
@@ -28,7 +28,7 @@ class XxeController @Inject()(cc: ControllerComponents)(implicit exec: Execution
   def put(id: String): Action[String] = Action.async(parse.tolerantText) { request =>
 
     val factory = SAXParserFactory.newInstance()
-        factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true)
+      //  factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true)
 
     val saxParser = factory.newSAXParser()
 
